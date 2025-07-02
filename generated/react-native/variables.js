d24e2fd2ad86803fed655250629e6ce6f3a14a10
Xavius123 <dantweigel@gmail.com>
2025-07-01 21:19:48 -0500

    fixed file structure

---
+++ generated/react-native/variables.js
@@ -0,0 +1,67 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const colorsBlack = "#000000";
+export const colorsWhite = "#ffffff";
+export const colorsOrange100 = "#fffaf0";
+export const colorsOrange200 = "#feebc8";
+export const colorsOrange300 = "#fbd38d";
+export const colorsOrange400 = "#f6ad55";
+export const colorsOrange500 = "#ed8936";
+export const colorsOrange600 = "#dd6b20";
+export const colorsOrange700 = "#c05621";
+export const colorsOrange800 = "#9c4221";
+export const colorsOrange900 = "#7b341e";
+export const shadowDropShadowsLightModeSm = {
+  color: "#0000001a",
+  type: "dropShadow",
+  x: 0,
+  y: 1,
+  blur: 4,
+  spread: 0,
+};
+export const shadowDropShadowsLightModeB = {
+  color: "#0000001a",
+  type: "dropShadow",
+  x: 0,
+  y: 4,
+  blur: 8,
+  spread: 0,
+};
+export const shadowDropShadowsLightModeM = {
+  color: "#0000001a",
+  type: "dropShadow",
+  x: 0,
+  y: 8,
+  blur: 16,
+  spread: 0,
+};
+export const shadowDropShadowsLightModeL = {
+  color: "#0000001a",
+  type: "dropShadow",
+  x: 0,
+  y: 12,
+  blur: 24,
+  spread: 0,
+};
+export const shadowDropShadowsLightModeXl = {
+  color: "#0000001a",
+  type: "dropShadow",
+  x: 0,
+  y: 16,
+  blur: 32,
+  spread: 0,
+};
+export const spacingSm = {
+  original: "4px",
+  number: 4,
+  decimal: 0.04,
+  scale: 64,
+};
+export const spacingMd = {
+  original: "8px",
+  number: 8,
+  decimal: 0.08,
+  scale: 128,
+};
