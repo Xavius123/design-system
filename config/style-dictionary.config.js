a17e99e9f102875581c0f9333806e0215e4ae0c4
Dan.Weigel <dan.weigel@nafinc.com>
2025-07-08 12:07:19 -0700

    added theme

--- config/style-dictionary.config.js
+++ config/style-dictionary.config.js
@@ -3,12 +3,23 @@ import { formats, transformGroups } from 'style-dictionary/enums';
 export default {
   source: ['tokens/*.json'],
   platforms: {
+    scss: {
+      transformGroup: 'scss',
+      buildPath: 'build/scss/',
+      files: [
+        {
+          destination: 'variables.scss',
+          format: 'scss/variables',
+          filter: (token) => token.filePath.includes('colors'),
+        },
+      ],
+    },
     scssLight: {
       transformGroup: transformGroups.scss,
       buildPath: 'build/scss/',
       files: [
         {
-          destination: '_variables-light.scss',
+          destination: 'theme/variables-light.scss',
           format: 'scss/variables',
           filter: (token) => token.filePath.includes('colors-light'),
         },
@@ -19,8 +30,26 @@ export default {
       buildPath: 'build/scss/',
       files: [
         {
-          destination: '_variables-dark.scss',
+          destination: 'theme/variables-dark.scss',
           format: 'scss/variables',
+          filter: (token) => token.filePath.includes('colors-dark'),
+        },
+      ],
+    },
+    css: {
+      transformGroup: transformGroups.css,
+      buildPath: 'build/css/',
+      files: [
+        {
+          destination: 'variables-light.css',
+          format: 'css/variables',
+          selector: ':root[data-theme="light"]',
+          filter: (token) => token.filePath.includes('colors-light'),
+        },
+        {
+          destination: 'variables-dark.css',
+          format: 'css/variables',
+          selector: ':root[data-theme="dark"]',
           filter: (token) => token.filePath.includes('colors-dark'),
         },
       ],
